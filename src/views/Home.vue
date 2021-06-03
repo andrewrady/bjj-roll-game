91bb8ff34acc5f81c4ab62704fe9f55ca68401c1
Andrew Rady <andrew.arsoftware@gmail.com>
2021-06-02 22:00:34 -0600

    init project with basic layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,14 +14,14 @@
       </ion-header>
     
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <start-menu @set-difficulty="setDifficulty" />
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
+import StartMenu from './StartMenu.vue';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
@@ -32,7 +32,18 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    StartMenu
+  },
+  data () {
+    return {
+      difficulty: null
+    }
+  },
+  methods: {
+    setDifficulty(value) {
+      this.difficulty = value;
+    }
   }
 });
 </script>
---
+++ src/views/StartMenu.vue
@@ -0,0 +1,38 @@
+<script>
+import { IonButton } from '@ionic/vue';
+
+export default {
+  name: 'StartMenu',
+  components: { IonButton },
+  data() {
+    return {
+      difficulty: {
+        easy: 'easy',
+        medium: 'medium',
+        hard: 'hard'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <ion-button 
+    expand="block" 
+    fill="solid" 
+    @click="$emit('set-difficulty', difficulty.easy)">
+    Easy
+  </ion-button>
+  <ion-button 
+    expand="block" 
+    fill="solid"
+    @click="$emit('set-difficulty', difficulty.medium)">
+    Medium
+  </ion-button>
+  <ion-button 
+    expand="block" 
+    fill="solid"
+    @click="$emit('set-difficulty', difficulty.hard)">
+    Hard
+  </ion-button>
+</template>
